f84684c97cde8ec4dd7203ab283a3366dd1f075a
Spectrum5 <toskusergio@gmail.com>
2023-02-22 22:56:40 +0100

    Aggiunti CDN: - Bootstrap - Axios - Vue3 - Fontawesome

--- index.php
+++ index.php
@@ -4,8 +4,24 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Vue3 -->
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+        <!-- Axios -->
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+        <!-- Fontawesome -->
+        <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+
+        <!-- Style CSS -->
+        <link rel="stylesheet" href="./css/style.css">
+
         <title>PHP ToDo List JSON</title>
     </head>
+
     <body>
         
     </body>
